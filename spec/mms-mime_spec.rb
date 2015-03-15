967e2cf8bccfa16ef9a56827e3d910c2063a13e9
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2015-03-15 14:50:38 +0100

    Upgrade to RSpec 3.2

--- spec/mms-mime_spec.rb
+++ spec/mms-mime_spec.rb
@@ -2,33 +2,33 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 ROOT_DIR = File.expand_path(File.dirname(__FILE__) + '/..')
 
-describe Mime::Mms::Parser, "when first created" do
+RSpec.describe Mime::Mms::Parser, "when first created" do
   it "should not be nil" do
     p = Mime::Mms::Parser.new
-    p.should_not be_nil
-    p.message.class.should == Mime::Mms::Message
-    p.message.subject.should be(nil)
+    expect(p).to_not be_nil
+    expect(p.message.class).to eq(Mime::Mms::Message)
+    expect(p.message.subject).to be_nil
   end
 end
 
-describe Mime::Mms::Parser, "when initialized from files" do
+RSpec.describe Mime::Mms::Parser, "when initialized from files" do
   %w(mms-1 mms-2 mms-3 mms-4 mms-5).each do |file|
     it "should parse file #{file}" do
       p = Mime::Mms::Parser.new :file => "#{ROOT_DIR}/spec/fixtures/#{file}.bin"
       m = p.parse
-      m.parts.should_not be_nil
-      m.parts.size.should > 0
+      expect(m.parts).to_not be_nil
+      expect(m.parts.size).to be > 0
 
-      m.xml_parts.should_not be_nil
-      m.xml_parts.size.should == 1
+      expect(m.xml_parts).not_to be_nil
+      expect(m.xml_parts.size).to eq(1)
 
-      m.image_parts.should_not be_nil
-      m.image_parts.size.should > 0
+      expect(m.image_parts).not_to be_nil
+      expect(m.image_parts.size).to be > 0
 
       unless file == "mms-3"
-        m.text_parts.should_not be_nil
-        m.text_parts.size.should > 0
-        m.text.should_not be_nil
+        expect(m.text_parts).not_to be_nil
+        expect(m.text_parts.size).to be > 0
+        expect(m.text).not_to be_nil
       end
     end
   end
@@ -36,19 +36,19 @@ describe Mime::Mms::Parser, "when initialized from files" do
   it "should extract subject, shortcode and number from binary-encoded file" do
     p = Mime::Mms::Parser.new :file => "#{ROOT_DIR}/spec/fixtures/mms-1.bin"
     m = p.parse
-    m.subject.should be_nil
-    m.from.should == "14155556666"
-    m.to.should == "43333"
+    expect(m.subject).to be_nil
+    expect(m.from).to eq("14155556666")
+    expect(m.to).to eq("43333")
   end
-  
+
   it "should extract subject, shortcode and number from base64 encoded file" do
     p = Mime::Mms::Parser.new :file => "#{ROOT_DIR}/spec/fixtures/mms-5.bin"
     m = p.parse
-    m.subject.should_not be_nil
-    m.subject.should == "Test group of messages"
-    m.text.should == "Test MM"
-    m.from.should == "77777"
-    m.to.should == "1"
+    expect(m.subject).not_to be_nil
+    expect(m.subject).to eq("Test group of messages")
+    expect(m.text).to eq("Test MM")
+    expect(m.from).to eq("77777")
+    expect(m.to).to eq("1")
     # File.open("image.jpg","w") { |f| f.write m.image_parts.first.body }
   end
 
